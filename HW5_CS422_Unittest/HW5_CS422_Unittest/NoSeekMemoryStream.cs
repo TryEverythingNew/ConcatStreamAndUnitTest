a8a5dc2f0e0a40f53645ee5883f3b391a777088d
xueliangsun <xueliang.sun@wsu.edu>
2016-10-07 21:09:09 -0700

    ConcatStream class for combining two streams
    1st stream has fixed length, 2nd can be expanded.
    The idea is to supply with a class for combining a seekable stream and a
    non-seekable stream

---
+++ HW5_CS422_Unittest/HW5_CS422_Unittest/NoSeekMemoryStream.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS422
+{
+    /// <summary>
+    /// Represents a memory stream that does not support seeking, but otherwise has
+    /// functionality identical to the MemoryStream class.
+    /// </summary>
+    public class NoSeekMemoryStream : MemoryStream
+    {
+        private byte[] mem;
+        private long position;
+
+        public NoSeekMemoryStream(byte[] buffer) // implement the first construction method, where directly copy buffer
+        {
+            mem = new byte[buffer.Length];
+            buffer.CopyTo(mem, 0);
+            position = 0;
+            Capacity = mem.Length;
+            SetLength(mem.Length);
+        }
+        public NoSeekMemoryStream(byte[] buffer, int offset, int count) // implement the second construction where use only part of buffer
+        {
+            //buffer.(mem, offset);
+            mem = new byte[count];
+            for(int i=0; i < count; i++)
+            {
+                mem[i] = buffer[i + offset];
+            }
+            position = 0;
+            Capacity = count;
+            SetLength(mem.Length);
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)// for read, you read bytes into buffer until read enough bits or get to the end
+        {
+            for(int i = 0; i < count; i++)
+            {
+                if (position >= Length)
+                    return i;
+                buffer[i + offset] = mem[position++];
+            }
+            return count;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (buffer == null) // check expcetion condition 
+                throw new ArgumentNullException();
+            if (offset + count > buffer.Length)
+                throw new ArgumentException();
+
+            for(int i = 0; i < count; i++)
+            {
+                if (position >= Length) // when writting, if max length met, we need to enlarge our byte buffer
+                {
+                    byte[] memnew = new byte[Length * 2];
+                    Capacity = (int) Length * 2;
+                    SetLength(Length * 2);
+                    mem.CopyTo(memnew, 0);  // copy the original buffer to new buffer
+                    mem = memnew;
+                }
+                mem[position++] = buffer[i + offset];
+            }
+            //base.Write(buffer, offset, count);
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return position;
+            }
+
+            set
+            {
+                throw new NotSupportedException(); // you are not allowed to set Position
+            }
+        }
+        // Override necessary properties and methods to ensure that this stream functions
+        // just like the MemoryStream class, but throws a NotSupportedException when seeking3
+        // is attempted (you'll have to override more than just the Seek function!)
+        public override bool CanSeek
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override long Seek(long offset, SeekOrigin loc)
+        {
+            if (CanSeek)
+            {
+                long pos_origin = (long)loc;
+                long start = 0;
+                switch (pos_origin)
+                {
+                    case 0: start = 0; break;
+                    case 1: start = Position; break;
+                    case 2: start = Length; break;
+                }
+                position = start + offset;
+                if(position >= Length)  // if we seek to some position larger than the buffer length, we enlarge the buffer
+                {
+                    byte[] memnew = new byte[position * 2];
+                    Capacity = (int)position * 2;
+                    SetLength(position * 2);
+                    mem.CopyTo(memnew, 0);  // copy the original buffer to new buffer
+                    mem = memnew;
+                }
+                return position;
+            }
+            else
+                throw new NotSupportedException();
+        }
+
+    }
+}
---
+++ HW5_CS422_Unittest/HW5_CS422_Unittest/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS422
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test two memory streams.
+            byte[] data1 = new byte[5]{ 1, 2, 3 , 4, 5 };
+            byte[] data2 = new byte[5] { 11, 12, 13, 14, 15 };
+            MemoryStream mem1 = new MemoryStream();
+            mem1.Write(data1, 0, data1.Length);
+            MemoryStream mem2 = new MemoryStream();
+            mem2.Write(data2,0,data2.Length);
+            ConcatStream cs1 = new ConcatStream(mem1, mem2);
+            byte[] test1 = new byte[1024];
+            Random a = new Random();
+            int offset = 0;
+            int count = 4;// you can set this value of count from 4 to any integer for unit testing
+            int read = cs1.Read(test1, offset, 10);
+            //cs1.Seek(0, SeekOrigin.Begin);
+            cs1.Position = 0;
+            read = cs1.Read(test1, offset, 10);
+            cs1.Write(data2, 0, data2.Length);
+            cs1.Position = 1;
+            read = cs1.Read(test1, offset, 10);
+
+            Console.WriteLine("expected value is start with offset of " + offset + " and read count is " + count);
+            for (int i = 0; i < read; i++)
+            {
+                Console.Write(test1[i+offset].ToString() + " ");
+            }
+            Console.WriteLine();
+
+            offset = a.Next(5);
+            count = 6; // you can set this value of count from 6 to any integer for unit testing
+            read = cs1.Read(test1, offset, count);
+
+            Console.WriteLine("expected value is start with offset of " + offset + " and read count is " + count);
+            for (int i = 0; i < read; i++)
+            {
+                Console.Write(test1[i + offset].ToString() + " ");
+            }
+            Console.WriteLine();
+
+            // unit test for reading concatenated stream with memory stream as the first stream and noseekmem one as the second
+            byte[] data3 = new byte[10] { 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+            NoSeekMemoryStream nmem1 = new NoSeekMemoryStream(data3);
+            ConcatStream cs2 = new ConcatStream(mem1, nmem1);
+            offset = a.Next(5);
+            count = 6;
+            read = cs2.Read(test1, offset, count); 
+
+            Console.WriteLine("expected value is start with offset of " + offset + " and read count is " + count);
+            for (int i = 0; i < read; i++)
+            {
+                Console.Write(test1[i + offset].ToString() + " ");
+            }
+            Console.WriteLine();
+
+            NoSeekMemoryStream nmem2 = new NoSeekMemoryStream(data3, offset, 10-offset);
+            ConcatStream cs3 = new ConcatStream(mem1, nmem2, 20);
+            offset = a.Next(5);
+            count = 6;
+            read = cs3.Read(test1, offset, count); 
+
+            Console.WriteLine("expected value is start with offset of " + offset + " and read count is " + count);
+            for (int i = 0; i < read; i++)
+            {
+                Console.Write(test1[i + offset].ToString() + " ");
+            }
+            Console.WriteLine();
+
+            // test the read / write functionality
+            NoSeekMemoryStream nmem3 = new NoSeekMemoryStream(data3, offset, 10 - offset);
+            cs3 = new ConcatStream(mem1, nmem3, 20);
+            cs3.Write(data3, 0, 10);
+            read = cs3.Read(test1, offset, count);
+            cs3.Write(data3, 0, 10);
+            read = cs3.Read(test1, offset, count);
+            cs3.Position = 0; // expecting exception
+            read = cs3.Read(test1, offset, count);
+            // test query the length property
+            long l = cs1.Length;
+            l = cs2.Length;
+            l = cs3.Length; // because nonmemorystream is similar to memorystream in length property, we expect that we can query the length
+
+            // check the seek function
+            cs1.Seek(1, SeekOrigin.Begin);  // expect working
+            cs2.Seek(1, SeekOrigin.Current); // expect exception due to nonmemorystream
+            cs3.Seek(1, SeekOrigin.End);    // expect exception due to nomemorystream
+        }
+    }
+}
